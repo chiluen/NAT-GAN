f8ae905e3553f3ff0a682e710e45adb7a9fbc922
chiluen <leo545035@gmail.com>
2020-03-24 15:51:43 +0800

    add LSTM discriminator

--- discriminator.py
+++ discriminator.py
@@ -56,6 +56,34 @@ class FairseqRNNClassifier(BaseFairseqModel):
 
         return output
 
+class LSTM(BaseFairseqModel):
+    def __init__(self, input_size, output_size, hidden_size, input_vocab):
+        super().__init__()
+        self.input_vocab = input_vocab
+        self.input_size = input_size  #在seq2seq NLP, 這就是len(input_vocab)
+        self.output_size = output_size
+        self.hidden_size = hidden_size
+
+        self.lstm = nn.LSTM(input_size, hidden_size, 1)  #LSTM(數據向量, 隱藏元向量, number of layer)
+        self.linear = nn.Linear(hidden_size, output_size)
+        #self.one_hot_inputs = torch.eye(len(input_vocab))
+        self.register_buffer('one_hot_inputs', torch.eye(len(input_vocab)))
+    def init_hidden(self, bsz):
+        h0 = torch.randn(1, bsz, self.hidden_size) #1是指number of layer
+        c0 = torch.randn(1, bsz, self.hidden_size)
+        return h0, c0
+
+    def forward(self, src_tokens):
+        bsz, _ = src_tokens.size()
+        h0, c0 = self.init_hidden(bsz)
+        input = self.one_hot_inputs[src_tokens.long()]# (bsz, seq-len, vocab-len)
+        input = input.permute(1,0,2) #(seq-len, bsz, vocab-len)
+        input = input.to(src_tokens.device)
+
+        output, _ = self.lstm(input, (h0,c0))
+        output = self.linear(output[-1], self.output_size) #這邊output[-1]其實就是hn
+        return output
+
 class Discriminator():
     def __init__(self, input_size, output_size, hidden_size, input_vocab):
         self.input_size = input_size
